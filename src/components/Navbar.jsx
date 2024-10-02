76ca03b96814886a1bb23f7c6723668964ce127c
Shiva Raj Paudel <official.beexoul@gmail.com>
2024-10-02 21:48:50 +0545

    Project in Process

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand"  href="#"><span className="badge bg-light text-dark fs-5"> World News  </span> </a>
+    <a className="navbar-brand"  href="/"><span className="badge bg-light text-dark fs-5"> World News  </span> </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,7 +2,7 @@
 const NewsItem = ( {title,description,src,url}) => {
   return (
     <div>
-      <div className="grid card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"380px"}}>
+      <div className="col-sm-6 display: flex; card-group mb-sm-0 card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"380px"}}>
     <img src={src} style={{height:"200px",width:"360px"  }} className="card-img-top" alt="..."/>
     <div className="card-body">
       <h5 className="card-title">{title.slice(0,50)}</h5>
